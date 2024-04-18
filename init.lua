9c0317877c15ef5c29b4fec311eb04c4c161de59
kylefabicon <ktfabicon@up.edu.ph>
2024-04-18 09:56:53 +0800

    update noice

--- init.lua
+++ init.lua
@@ -565,14 +565,14 @@ require('lazy').setup({
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 500,
+          timeout_ms = 5000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
         blade = { 'blade-formatter', 'rustywind' },
-        php = { 'php' },
+        php = { 'phpcsfixer' },
         javascript = { 'prettierd' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
@@ -583,7 +583,7 @@ require('lazy').setup({
       },
 
       formatters = {
-        php = {
+        phpcsfixer = {
           command = 'php-cs-fixer',
           args = {
             'fix',
@@ -909,6 +909,22 @@ require('lazy').setup({
     event = 'VeryLazy',
     opts = {
       -- add any options here
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
